31f87a2cbf874cb58c076a90bdaa7cb1e9222151
wycliff-ochieng <wyckieochieng91@gmail.com>
2025-09-28 20:06:07 +0300

    server entrypoint

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -1,7 +1,39 @@
 package api
 
-import "fmt"
+import (
+	"fmt"
+	"log/slog"
+	"net/http"
 
-func Run() {
-	fmt.Println("spining up server and router")
+	"github.com/gorilla/mux"
+)
+
+type Server struct {
+	l    *slog.Logger
+	addr string
+}
+
+func NewServer(l *slog.Logger, addr string) *Server {
+	return &Server{
+		l:    l,
+		addr: addr,
+	}
+}
+
+func LociTest(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("TEsting the server")
+}
+
+func (s *Server) Run() {
+	fmt.Println("starting up run method")
+
+	router := mux.NewRouter()
+
+	test := router.Methods("POST").Subrouter()
+	test.HandleFunc("/", LociTest)
+
+	if err := http.ListenAndServe(s.addr, router); err != nil {
+		fmt.Errorf("rror listening to server: %s", err)
+	}
+
 }
--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log/slog"
+	"os"
 
+	"github.com/wycliff-ochieng/cmd/api"
+)
 
 func main() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 	fmt.Println("Spinning up the LOCI APPLICATION")
+	server := api.NewServer(logger, ":3000")
+	server.Run()
 }
