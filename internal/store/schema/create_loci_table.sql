00b776d6a4871daebfdeb49348f7ddc7b20502e4
wycliff-ochieng <wyckieochieng91@gmail.com>
2025-10-01 19:58:37 +0300

    add: user authentication,register,login functionality

---
+++ internal/store/schema/create_loci_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE loci (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    message TEXT NOT NULL CHECK (length(message) > 0 AND length(message) <= 280),
+    -- GEOGRAPHY type is better than GEOMETRY for lat/lon calculations in meters/km
+    location GEOGRAPHY(Point, 4326) NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    view_count INT NOT NULL DEFAULT 0
+);
