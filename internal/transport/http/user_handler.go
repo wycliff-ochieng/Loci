d467276a07e1f85d8cf2472e7c56cd26868ebd2e
wycliff-ochieng <wyckieochieng91@gmail.com>
2025-10-03 22:01:15 +0300

    add: register user handler function

--- internal/transport/http/user_handler.go
+++ internal/transport/http/user_handler.go
@@ -13,6 +13,14 @@ type UserHandler struct {
 	us     *service.UserService
 }
 
+type RegisterReq struct {
+	Username  string
+	Firstname string
+	Lastname  string
+	Email     string
+	Password  string
+}
+
 func NewUserHandler(l *slog.Logger, us *service.UserService) *UserHandler {
 	return &UserHandler{
 		logger: l,
@@ -23,5 +31,30 @@ func NewUserHandler(l *slog.Logger, us *service.UserService) *UserHandler {
 func (h *UserHandler) Register(w http.ResponseWriter, r *http.Request) {
 	h.logger.Info("Register user handler touched ,sending data to service layer")
 
-	err := json.NewDecoder(r.Body).Decode()
+	ctx := r.Context()
+
+	var RegisterUser *RegisterReq
+
+	err := json.NewDecoder(r.Body).Decode(&RegisterUser)
+	if err != nil {
+		http.Error(w, "failed to register user for some reason", http.StatusExpectationFailed)
+		return
+	}
+
+	if RegisterUser.Username == "" || RegisterUser.Firstname == "" || RegisterUser.Lastname == "" || RegisterUser.Email == "" {
+		http.Error(w, "some requireed fields are missing values", http.StatusExpectationFailed)
+		return
+	}
+
+	user, err := h.us.Register(ctx, RegisterUser.Username, RegisterUser.Firstname, RegisterUser.Lastname, RegisterUser.Email, RegisterUser.Password)
+	if err != nil {
+		http.Error(w, "service layer failure: ", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(&user)
+
+	//err := json.NewDecoder(r.Body).Decode()
 }
