928440e9092f20d3dc54121ea17391db257cc68f
wycliff-ochieng <wyckieochieng91@gmail.com>
2025-10-03 23:55:01 +0300

    loading configuration, config injection into the NewServer constructor

--- cmd/main.go
+++ cmd/main.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"log/slog"
 	"os"
 
 	"github.com/wycliff-ochieng/api"
+	"github.com/wycliff-ochieng/internal/config"
 )
 
 func main() {
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	conf, err := config.Load()
+	if err != nil {
+		log.Fatal("failed to load env")
+	}
 	fmt.Println("Spinning up the LOCI APPLICATION")
-	server := api.NewServer(logger, ":3000")
+	server := api.NewServer(logger, ":3000", conf)
 	server.Run()
 }
