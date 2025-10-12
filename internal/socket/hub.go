e648ddbfe76b7551df8b23ba2ca038337110d72c
wycliff-ochieng <wyckieochieng91@gmail.com>
2025-10-12 13:01:51 +0300

    add: client, hub, broadcast to clients,read and write pump

---
+++ internal/socket/hub.go
@@ -0,0 +1,99 @@
+package socket
+
+import (
+	"encoding/json"
+	"log"
+
+	"github.com/google/uuid"
+	"github.com/gorilla/websocket"
+	"github.com/wycliff-ochieng/internal/models"
+	"github.com/wycliff-ochieng/sqlc"
+)
+
+type Client struct {
+	Hub      *Hub
+	Conn     *websocket.Conn
+	Send     chan []byte
+	UserID   uuid.UUID
+	Location *models.GeoPoint
+}
+
+type Hub struct {
+	Clients        map[*Client]bool
+	Register       chan *Client
+	UnRegister     chan *Client
+	Broadcast      chan []byte
+	BroadcastLocus chan *models.Locus
+}
+
+type Websocket struct {
+	Type    string      `json:"type"`
+	Payload interface{} `json:"payload"`
+}
+
+func (h *Hub) Run() {
+
+	for {
+		select {
+		case client := <-h.Register:
+			//map[*Client] bool = client -> add client to map
+			h.Clients[client] = true
+
+		case client := <-h.UnRegister:
+			//check if client exists in the map
+			if _, ok := h.Clients[client]; ok {
+
+				//delete if present then close
+				delete(h.Clients, client)
+
+				close(client.Send)
+
+			}
+		case locus := <-h.BroadcastLocus:
+			//prepare outbound message
+			message := Websocket{
+				Type:    "LOCUS_NEW",
+				Payload: locus,
+			}
+
+			//unmarshal the message
+			marshalledMessage, err := json.Marshal(message)
+			if err != nil {
+				log.Printf("error marshaling message due to: %s", err)
+				continue
+			}
+
+			for c := range h.Clients {
+				if c.Location == nil {
+					continue // skip clients with no location
+				}
+
+				locusPoint := &models.GeoPoint{
+					Lat:  locus.Location.Lat,
+					Long: locus.Location.Long,
+				}
+
+				//calculate distance btw client and loci location
+
+				dist := models.CalculateDistance(c.Location, locusPoint)
+
+				//set broadcast distance -> distance allowed to see the message
+				const broadcastDistance = 5000
+
+				if dist < broadcastDistance {
+					select {
+					case c.Send <- marshalledMessage:
+					default:
+						close(c.Send)
+						delete(h.Clients, c)
+					}
+				}
+			}
+		}
+
+	}
+}
+
+func (h *Hub) BroadcastNewLoci(locus *sqlc.Loci) {
+	return
+}
