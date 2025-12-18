8e0784d4724ecd8507741976ae8594200ab211ad
wycliff-ochieng <wyckieochieng91@gmail.com>
2025-12-19 00:23:30 +0300

    faet: sqlc changes

--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -6,11 +6,16 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"time"
 
-	"github.com/gofrs/uuid"
+	"github.com/google/uuid"
 	"github.com/wycliff-ochieng/internal/limitter"
 	"github.com/wycliff-ochieng/internal/models"
 	"github.com/wycliff-ochieng/internal/store"
+	"github.com/wycliff-ochieng/pkg/auth"
+	"golang.org/x/crypto/bcrypt"
+
+	//"github.com/wycliff-ochieng/pkg/middleware/auth"
 	"github.com/wycliff-ochieng/sqlc"
 )
 
@@ -29,10 +34,11 @@ const (
 )
 
 var (
-	ErrForbidden   = errors.New("Cannot perform this operation")
-	ErrOutOfBounds = errors.New("Error setting bounds")
-	ErrNotFound    = errors.New("No user with that email/ username")
-	ErrRateLimited = errors.New("user not allowed to create new locus, maximum retires exceeded")
+	ErrForbidden       = errors.New("Cannot perform this operation")
+	ErrOutOfBounds     = errors.New("Error setting bounds")
+	ErrNotFound        = errors.New("No user with that email/ username")
+	ErrRateLimited     = errors.New("user not allowed to create new locus, maximum retires exceeded")
+	ErrInvalidPassword = errors.New("wrong password input")
 )
 
 func NewUserService(db *store.Postgis, query sqlc.Queries) *UserService {
@@ -54,20 +60,27 @@ func (us *UserService) Register(ctx context.Context, username string, firstname 
 		return nil, ErrForbidden
 	}
 
+	harshedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
+
+	newUserID := uuid.New()
+
 	err = us.query.CreateUser(ctx, sqlc.CreateUserParams{
-		//ID:           ID,
+		ID:           newUserID,
 		Username:     username,
 		Firstname:    firstname,
 		Lastname:     lastname,
 		Email:        email,
-		PasswordHash: password,
+		PasswordHash: string(harshedPassword),
 		//CreatedAt:    createdat,
 	})
 	if err != nil {
 		return nil, err
 	}
 	return &models.UserResponse{
-		//UserID:    ID,
+		UserID:    newUserID,
 		Firstname: firstname,
 		Lastname:  lastname,
 		Username:  username,
@@ -75,21 +88,45 @@ func (us *UserService) Register(ctx context.Context, username string, firstname 
 	}, nil
 }
 
-func (us *UserService) LoginUser(ctx context.Context, username string, email string, password string) (*models.UserResponse, error) {
+func (us *UserService) LoginUser(ctx context.Context, username string, email string, password string) (*auth.TokenPair, *models.UserResponse, error) {
 
 	//sqlc.LoginParams
-	var User models.User
+	//var User models.User
+	log.Println("user service login function touched")
 
 	user, err := us.query.Login(ctx, sqlc.LoginParams{Email: email, Username: username})
-	if err != nil || err == sql.ErrNoRows {
-		return nil, ErrNotFound
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil, err
+		}
+		log.Printf("error due to: %s", err)
+		return nil, nil, err
 	}
 
-	if err := User.ComparePassword(password); err != nil {
-		return nil, fmt.Errorf("invalid password!!Try again")
+	//if err := User.ComparePassword(password); err != nil {
+	//	return nil, nil, fmt.Errorf("invalid password!!Try again")
+	//}
+
+	if err := models.ComparePassword(user.PasswordHash, password); err != nil {
+		log.Printf("comparing password error: %s", err)
+		return nil, nil, fmt.Errorf("invalid password")
 	}
 
-	return &models.UserResponse{
+	token, err := auth.GenerateTokenPair(
+		user.ID,
+		//user.UserID,
+		//role,
+		user.Email,
+		string(auth.JWTsecret),     //jwtsecret
+		string(auth.Refreshsecret), //refreshsecret
+		time.Hour*24,
+		time.Hour*24*7,
+	)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to generate tokens: %v", err)
+	}
+
+	return token, &models.UserResponse{
 		UserID:    user.ID,
 		Firstname: user.Firstname,
 		Lastname:  user.Lastname,
@@ -102,11 +139,13 @@ func (us *UserService) LoginUser(ctx context.Context, username string, email str
 func (us *UserService) GetLociWithinBounds(ctx context.Context, bounds models.BoundBox) ([]sqlc.GetLociInBoundsRow, error) {
 	log.Println("Database operation in motion")
 
+	log.Printf("[SERVICE] Received BoundingBox: %+v", bounds)
+
 	messages, err := us.query.GetLociInBounds(ctx, sqlc.GetLociInBoundsParams{
-		StMakeenvelope:   bounds.NorthEastLat,
-		StMakeenvelope_2: bounds.NorthEastLong,
-		StMakeenvelope_3: bounds.SouthWestLat,
-		StMakeenvelope_4: bounds.SouthWestLong,
+		StMakeenvelope:   bounds.SouthWestLong,
+		StMakeenvelope_2: bounds.SouthWestLat,
+		StMakeenvelope_3: bounds.NorthEastLong,
+		StMakeenvelope_4: bounds.NorthEastLat,
 	})
 	if err != nil {
 		return nil, err
@@ -138,9 +177,11 @@ func (us *UserService) CreateLoci(ctx context.Context, userID uuid.UUID, params 
 	//content moderation logic -> check if message contains only allowed words
 
 	dbparams := sqlc.CreateLociParams{
-		UserID:   params.UserID,
-		Message:  params.Message,
-		Location: params.Location,
+		UserID:        params.UserID,
+		Message:       params.Message,
+		StMakepoint:   params.StMakepoint,
+		StMakepoint_2: params.StMakepoint_2,
+		//Location: params.Location,
 	}
 	//calling the db
 	loci, err := us.query.CreateLoci(ctx, dbparams)
